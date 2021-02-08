551bf5b1b7bc69f22664fcdc6a6a8df0b12d3af3
Ka-Bis <k.sydaphasavanh@gmail.com>
2021-02-08 16:19:31 +0100

    dynamic breadcrumb

--- src/components/Navigation/Breadcrumb.vue
+++ src/components/Navigation/Breadcrumb.vue
@@ -1,44 +1,34 @@
 <template>
   <div>
-    <!-- <nav aria-label="breadcrumb" class="ariane">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item"><a href="#">Library</a></li>
-        <li class="breadcrumb-item active">Data</li>
-      </ol>
-    </nav> -->
-        
     <b-breadcrumb class="ariane">
-      <b-breadcrumb-item><router-link to="/">Home</router-link></b-breadcrumb-item>
-      <b-breadcrumb-item>Foo</b-breadcrumb-item>
-      <b-breadcrumb-item>Bar</b-breadcrumb-item>
-      <b-breadcrumb-item>Baz</b-breadcrumb-item>
+      <b-breadcrumb-item v-for="(p, index) of table" v-bind:href="paths[index]" v-bind:key="p">{{ table[index] }}
+      </b-breadcrumb-item>
     </b-breadcrumb>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Breadcrumb",
-};
+  export default {
+    name: "Breadcrumb",
+    computed: {
+      table: function () {
+        return this.$router.currentRoute.fullPath.split("/").splice(1);
+      },
+      paths: function () {
+        let result = [this.table.length];
+        result[0] = "/" + this.table[0];
+        for (let i = 1; i < this.table.length; i++) {
+          result[i] = result[i - 1] + "/" + this.table[i];
+        }
+        return result;
+      },
+    },
+  };
 </script>
 
 <style scoped>
-.ariane {
-  /* margin-left: 11em; */
-  float: right;
-  /* background-color: inherit; */
-}
-
-.breadcrumb {
-  background-color: inherit;
-}
-@media (max-width: 990px) {
   .ariane {
-    margin: 0;
-    float: left;
-    /* background-color: rgba(173, 171, 171, 0.171); */
-    border-radius: 0;
+    margin-left: 11em;
+    background-color: inherit;
   }
-}
 </style>
