8648eb110fbde6ce9d400b2caf92a771695ef643
Ka-Bis <k.sydaphasavanh@gmail.com>
2021-02-08 16:17:00 +0100

    add change to course card

--- src/components/Formateur/CourseCard.vue
+++ src/components/Formateur/CourseCard.vue
@@ -1,76 +1,7 @@
 <template>
   <div class="container">
     <h2 class="text-left">Derniers cours</h2>
-    <!-- V1 WITH BOOTSTRAP -->
-    <!-- <div class="card-menu">
-      <div class="card card-course">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div>
-
-      <div class="card card-course">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div>
-
-      <div class="card card-course">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
-      </div>
-    </div> -->
-
-    <!-- V2 WITH BOOTSTRAP-VUE -->
-    <!-- <b-card title="Card title" sub-title="Card subtitle" class="card-course">
-        <b-card-text>
-          Some quick example text to build on the <em>card title</em> and make
-          up the bulk of the card's content.
-        </b-card-text>
-        <a href="#" class="card-link">Card link</a>
-        <b-link href="#" class="card-link">Another link</b-link>
-      </b-card>
-
-      <b-card title="Card title" sub-title="Card subtitle" class="card-course">
-        <b-card-text>
-          Some quick example text to build on the <em>card title</em> and make
-          up the bulk of the card's content.
-        </b-card-text>
-        <a href="#" class="card-link">Card link</a>
-        <b-link href="#" class="card-link">Another link</b-link>
-      </b-card>
-
-      <b-card title="Card title" sub-title="Card subtitle" class="card-course">
-        <b-card-text>
-          Some quick example text to build on the <em>card title</em> and make
-          up the bulk of the card's content.
-        </b-card-text>
-        <a href="#" class="card-link">Card link</a>
-        <b-link href="#" class="card-link">Another link</b-link>
-      </b-card> -->
-
+   
     <!-- V3 WITH BOOTSTRAP-VUE AND DATA BIDIND IN A LOOP -->
     <div class="card-menu row-cols-1 row-cols-md-3">
       <div v-for="course in courses" :key="course.id">
@@ -79,6 +10,10 @@
             {{ course.text }}
           </b-card-text>
           <a href="#" class="card-link">{{ course.link }}</a>
+          <template #footer>
+            <small>{{ course.date |  moment('timezone', 'Europe/Paris', 'dddd DD MMMM YYYY HH:mm') }}</small>
+            <!-- TODO : modifier la langue de la date -->
+          </template>
         </b-card>
       </div>
     </div>
@@ -97,6 +32,7 @@
             text: `Some quick example text to build on the card title 
                 and make up the bulk of the card's content.`,
             link: "Detail course link",
+            date: new Date("02/12/2021"),
           },
           {
             id: 2,
@@ -105,6 +41,7 @@
             text: `Some quick example text to build on the card title 
                 and make up the bulk of the card's content.`,
             link: "Detail course link",
+            date: new Date(),
           },
           {
             id: 3,
@@ -113,6 +50,7 @@
             text: `Some quick example text to build on the card title 
                 and make up the bulk of the card's content.`,
             link: "Detail course link",
+            date: new Date(),
           },
           {
             id: 4,
@@ -121,6 +59,7 @@
             text: `Some quick example text to build on the card title 
                 and make up the bulk of the card's content.`,
             link: "Detail course link",
+            date: new Date(),
           },
         ],
       };
@@ -134,7 +73,7 @@
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-    justify-content: center;
+    /* justify-content: center; */
   }
 
   .card-menu>div[class*="card-course"] {
@@ -143,7 +82,7 @@
 
   .card-course {
     width: 18rem;
-    /* height: 18rem; */
+    height: 16rem;
     /* margin-left: 1em;
     margin-right: 1em; */
     margin: 1em auto;
