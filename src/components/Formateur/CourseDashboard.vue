a6938dec55ffa92ab3b3a84d2710338bc01ecf8c
Ka-Bis <k.sydaphasavanh@gmail.com>
2021-02-11 00:48:28 +0100

    table with mock data implemend / create pagination for table

--- src/components/Formateur/CourseDashboard.vue
+++ src/components/Formateur/CourseDashboard.vue
@@ -1,107 +1,181 @@
 <template>
-    <div>
-        <h1>Liste des cours</h1>
-        <section>
-            <div class="container">
-                <div class="row">
-                    <div class="mb-3 ml-auto">
-                        <b-dropdown text="Trier par" size="sm" variant="none">
-                            <b-dropdown-item href="#">Intitulé</b-dropdown-item>
-                            <b-dropdown-item href="#">Date</b-dropdown-item>
-                        </b-dropdown>
-                    </div>
-                    <table class="table table-sm course-table">
-                        <thead>
-                            <tr>
-                                <th>Intitulé</th>
-                                <th>Date</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="course in courses" :key="course.id">
-                                <td>
-                                    <router-link to="/" class="text-dark">
-                                        {{ course.title }}
-                                    </router-link>
-                                </td>
-                                <td>{{ course.date | moment("DD/MM/YYYY") }}</td>
-                                <td class="mx-auto">
-                                    <img src="@/assets/icons/pencil.svg" class="icon" alt="edit" @click="isClick()" />
-                                    <img src="@/assets/icons/close.svg" class="icon" alt="remove" @click="isClick()" />
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <router-link class="btn btn-outline-secondary rounded-0 btn-sm ml-auto"
-                        :to="{ name: 'add-course' }">Ajouter un cours</router-link>
-                </div>
-            </div>
-        </section>
-    </div>
+  <div>
+    <h1>Liste des cours</h1>
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="mb-3 ml-auto">
+            <b-dropdown
+              text="Trier par date"
+              size="sm"
+              variant="none"
+              class="drpdwn"
+            >
+              <b-dropdown-item href="#">Croissant</b-dropdown-item>
+              <b-dropdown-item href="#">Decroissant</b-dropdown-item>
+            </b-dropdown>
+
+            <b-dropdown
+              text="Trier par Intitulé"
+              size="sm"
+              variant="none"
+              class="drpdwn"
+            >
+              <b-dropdown-item href="#">Croissant</b-dropdown-item>
+              <b-dropdown-item href="#">Decroissant</b-dropdown-item>
+            </b-dropdown>
+          </div>
+          <div class="offset-md-1 col-10 offset-md-1">
+            <b-table
+              id="my-table"
+              responsive
+              striped
+              fixed
+              small
+              :items="courses"
+              :fields="fields"
+              :per-page="perPage"
+              :current-page="currentPage"
+            >
+              <template #cell(index)="data">
+                {{ data.index + 1 }}
+              </template>
+              <template #cell(title)="data">
+                <b-link href="#" style="color:black;">
+                  {{ data.value }}
+                </b-link>
+              </template>
+              <template #cell(edit)>
+                <font-awesome-icon
+                  :icon="['fas', 'pen']"
+                  class="icon text-secondary"
+                />
+              </template>
+              <template #cell(remove)>
+                <font-awesome-icon
+                  :icon="['fas', 'times']"
+                  class="icon text-danger"
+                  @click="deleteItem(index)"
+                />
+              </template>
+              <template #cell(dateBegin)="data">
+                {{
+                  data.value | moment("timezone", "Europe/Paris", "DD/MM/YYYY")
+                }}
+              </template>
+              <template #cell(dateEnd)="data">
+                {{
+                  data.value | moment("timezone", "Europe/Paris", "DD/MM/YYYY")
+                }}
+              </template>
+            </b-table>
+          </div>
+          <div class="ml-auto">
+            <a class="btn rounded-0 btn-sm ml-auto mb-2" :href="add_course">
+              Ajouter un cours
+            </a>
+          </div>
+        </div>
+        <b-pagination
+          class="pages text-dark"
+          v-model="currentPage"
+          :total-rows="rows"
+          :per-page="perPage"
+          aria-controls="my-table"
+          size="sm"
+        >
+        </b-pagination>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Course",
-        methods: {
-            isClick() {
-                console.log("clicked");
-            },
+// import { CourseModel } from "@/utils/Course.js";
+import CourseModel from "@/mock/course.json";
+export default {
+  name: "Course",
+  data() {
+    return {
+      courses: CourseModel,
+      fields: [
+        "index",
+        // {
+        //   key: "id",
+        //   label: "#",
+        // },
+        {
+          key: "title",
+          label: "Intitulé",
+          sortable: true,
         },
-        data() {
-            const variables = {
-                fields: ["title", "date"],
-                courses: [{
-                        id: 1,
-                        title: "Course title",
-                        subtitle: "Course subtitle",
-                        text: `Some quick example text to build on the card title 
-                        and make up the bulk of the card's content.`,
-                        link: "Detail course link",
-                        date: new Date(),
-                    },
-                    {
-                        id: 2,
-                        title: "Course title",
-                        subtitle: "Course subtitle",
-                        text: `Some quick example text to build on the card title 
-                        nd make up the bulk of the card's content.`,
-                        link: "Detail course link",
-                        date: new Date(),
-                    },
-                    {
-                        id: 3,
-                        title: "Course title",
-                        subtitle: "Course subtitle",
-                        text: `Some quick example text to build on the card title 
-                        and make up the bulk of the card's content.`,
-                        link: "Detail course link",
-                        date: new Date(),
-                    },
-                    {
-                        id: 4,
-                        title: "Course title",
-                        subtitle: "Course subtitle",
-                        text: `Some quick example text to build on the card title 
-                        and make up the bulk of the card's content.`,
-                        link: "Detail course link",
-                        date: new Date(),
-                    },
-                ],
-            };
-            return variables;
+        {
+          key: "dateBegin",
+          label: "Date de debut",
+          sortable: true,
         },
+        {
+          key: "dateEnd",
+          label: "Date de fin",
+          sortable: true,
+        },
+        "edit",
+        "remove",
+      ],
+      perPage: 10,
+      currentPage: 1,
+      add_course: "/cours/ajouter-un-cours",
     };
+  },
+  methods: {
+    deleteItem(index) {
+      this.courses.splice(index, 1);
+    },
+  },
+  computed: {
+    rows() {
+      return this.courses.length;
+    },
+  },
+};
 </script>
-
 <style scoped>
-    .icon {
-        width: 20px;
-        margin: 0 1em;
-    }
+.drpdwn {
+  outline: green;
+}
 
-    .icon:hover {
-        cursor: pointer;
-    }
+.drpdwn:focus {
+  outline: red;
+}
+
+.icon {
+  /* width: 30px; */
+  margin: 0 0.5em;
+}
+
+.icon:hover {
+  cursor: pointer;
+}
+
+.btn {
+  border: 1px solid black;
+  outline: none;
+}
+
+.btn:focus {
+  outline: 0;
+}
+
+.btn:hover {
+  background-color: inherit;
+  color: inherit;
+}
+
+.pages:focus {
+  outline: red;
+}
+
+.page-item > button {
+  background-color: red;
+}
 </style>
