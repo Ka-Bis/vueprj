a5739e10618d5207612580001b937b3ccc9bf982
Ka-Bis <k.sydaphasavanh@gmail.com>
2021-02-11 00:50:19 +0100

    add plugins in main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import Vue from "vue";
 import "./plugins/bootstrap-vue";
+import "./plugins/fontawesome-vue"
+import "./plugins/moment-vue";
+import "./plugins/vue-dataTable";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VueMoment from "vue-moment";
-import moment from "moment-timezone";
+import vuejquery from 'vue-jquery';
 
-Vue.use(VueMoment, { moment });
+Vue.use(vuejquery)
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: (h) => h(App)
 }).$mount("#app");
