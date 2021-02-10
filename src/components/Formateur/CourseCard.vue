8c34ee6e4289c1787784eab101d6e4b863f9e4e3
Ka-Bis <k.sydaphasavanh@gmail.com>
2021-02-11 00:56:02 +0100

    update course card

--- src/components/Formateur/CourseCard.vue
+++ src/components/Formateur/CourseCard.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="container">
     <h2 class="text-left">Derniers cours</h2>
-   
+
     <!-- V3 WITH BOOTSTRAP-VUE AND DATA BIDIND IN A LOOP -->
     <div class="card-menu row-cols-1 row-cols-md-3">
-      <div v-for="course in courses" :key="course.id">
-        <b-card :title="course.title" :sub-title="course.subtitle" class="card-course">
-          <b-card-text>
+      <div v-for="course in courses.slice(0, 6)" :key="course.id">
+        <b-card :title="course.title" class="card-course">
+          <b-card-text class="text">
             {{ course.text }}
           </b-card-text>
-          <a href="#" class="card-link">{{ course.link }}</a>
-          <template #footer>
-            <small>{{ course.date |  moment('timezone', 'Europe/Paris', 'dddd DD MMMM YYYY HH:mm') }}</small>
-            <!-- TODO : modifier la langue de la date -->
-          </template>
+          <div>
+            <p>Du {{course.dateBegin | moment("timezone","Europe/Paris","DD/MM/YYYY")}} au
+              {{course.dateEnd | moment("timezone","Europe/Paris","DD/MM/YYYY")}}</p>
+          </div>
+          <p>{{course.description}}</p>
+          <a :href="course.id" class="card-link">Lien du cours</a>
         </b-card>
       </div>
     </div>
@@ -21,47 +22,13 @@
 </template>
 
 <script>
+  // import { CourseModel } from "@/utils/Course.js";
+  import CourseModel from "@/mock/course.json";
   export default {
     name: "CourseCard",
     data() {
       const variables = {
-        courses: [{
-            id: 1,
-            title: "Course title",
-            subtitle: "Course subtitle",
-            text: `Some quick example text to build on the card title 
-                and make up the bulk of the card's content.`,
-            link: "Detail course link",
-            date: new Date("02/12/2021"),
-          },
-          {
-            id: 2,
-            title: "Course title",
-            subtitle: "Course subtitle",
-            text: `Some quick example text to build on the card title 
-                and make up the bulk of the card's content.`,
-            link: "Detail course link",
-            date: new Date(),
-          },
-          {
-            id: 3,
-            title: "Course title",
-            subtitle: "Course subtitle",
-            text: `Some quick example text to build on the card title 
-                and make up the bulk of the card's content.`,
-            link: "Detail course link",
-            date: new Date(),
-          },
-          {
-            id: 4,
-            title: "Course title",
-            subtitle: "Course subtitle",
-            text: `Some quick example text to build on the card title 
-                and make up the bulk of the card's content.`,
-            link: "Detail course link",
-            date: new Date(),
-          },
-        ],
+        courses: CourseModel,
       };
       return variables;
     },
